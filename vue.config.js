2dc050632b7f252c3f954ed2575c137de188a1f3
27meisuto <27meisuto@gmail.com>
2022-09-16 11:59:44 +0900

    config 修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/kakeibo01-ver1.0' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
